7153bdcd3f430e365e2af66d52cba7f3d29b32ca
GravityAnn <168503880+GravityAnn@users.noreply.github.com>
2024-08-19 00:55:48 +0900

    Add files via upload

---
+++ bring_fgi.py
@@ -0,0 +1,46 @@
+from datetime import datetime, date, timedelta
+import math
+
+import sys
+sys.path.append(r'C:\\Python312\\Lib\\site-packages\\fear_and_greed')
+
+import pandas as pd
+import fear_and_greed
+import yfinance as yf
+
+def get_fear_and_greed():
+    fg = fear_and_greed.get()
+    fg_score = float(fg[0])
+    fg_score = round(fg_score,2)
+    fg_status = fg[1]
+
+    return (fg_score, fg_status)
+
+def buy_stock(ticker):
+    print("Buy some tqqq")
+    #slackout("Buy some tqqq")
+
+def sell_stock(ticker):
+    print("Sell some tqqq")
+    #slackout("Sell some tqqq")
+
+def main():
+    sell_score = 75
+    buy_score = 25
+
+    try:
+        fg_score, fg_status = get_fear_and_greed()
+
+        if fg_score > sell_score:
+            sell_stock()
+        if fg_score > sell_score:
+            buy_stock()
+
+        print(fg_score, fg_status)
+
+    except Exception as ex:
+        print("예외처리 :" + str(ex))
+
+if __name__ == "__main__":
+    main()
+
